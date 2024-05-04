0ee2d8a64ca03acd6732ba0df0ca3d729a6cd05e
congvt <cong.vt@zinza.com.vn>
2024-05-04 18:16:10 +0700

    handle build component footer

--- frontend-app/src/components/Layouts/Footer/Footer.js
+++ frontend-app/src/components/Layouts/Footer/Footer.js
@@ -1,5 +1,129 @@
 import React from "react";
-
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faPhone,
+  faEnvelope,
+  faLocation,
+  faEarth,
+  faArrowRight,
+} from "@fortawesome/free-solid-svg-icons";
+import {
+  faFacebook,
+  faTwitter,
+  faYoutube,
+  faInstagram,
+} from "@fortawesome/free-brands-svg-icons";
 export default function Footer() {
-  return <div>Footer</div>;
+  return (
+    <>
+      <div className="App-footer-contact-container">
+        <div className="App-footer-contact">
+          <h6>Thông tin liên hệ</h6>
+          <div className="d-flex flex-column">
+            <div className="d-flex align-items-center">
+              <FontAwesomeIcon icon={faLocation} />
+              <span className="p-2">175 CMT8, Phú My Hòa, Cà Mà, Hanoi</span>
+            </div>
+            <div className="d-flex align-items-center">
+              <FontAwesomeIcon icon={faPhone} />
+              <span className="p-2 fz-13">0971765824</span>
+            </div>
+            <div className="d-flex align-items-center">
+              <FontAwesomeIcon icon={faEnvelope} />
+              <span className="p-2 fz-13">congvtc02@gmail</span>
+            </div>
+            <div className="d-flex align-items-center">
+              <FontAwesomeIcon icon={faEarth} />
+              <span className="p-2 fz-13">ngoaingutinhoc.tech.com</span>
+            </div>
+          </div>
+        </div>
+        <div className="App-footer-contact">
+          <h6>Thông tin chung</h6>
+          <div className="d-flex flex-column">
+            <div className="d-flex align-items-center">
+              <FontAwesomeIcon icon={faArrowRight} />
+              <span className="p-2">Trang chủ</span>
+            </div>
+            <div className="d-flex align-items-center">
+              <FontAwesomeIcon icon={faArrowRight} />
+              <span className="p-2">Tin tức</span>
+            </div>
+            <div className="d-flex align-items-center">
+              <FontAwesomeIcon icon={faArrowRight} />
+              <span className="p-2">Đào tạo</span>
+            </div>
+            <div className="d-flex align-items-center">
+              <FontAwesomeIcon icon={faArrowRight} />
+              <span className="p-2">Thông báo</span>
+            </div>
+            <div className="d-flex align-items-center">
+              <FontAwesomeIcon icon={faArrowRight} />
+              <span className="p-2">Liên hệ</span>
+            </div>
+          </div>
+        </div>
+        <div className="App-footer-contact">
+          <h6>Các lĩnh vực hoạt động</h6>
+          <div className="d-flex flex-column">
+            <div className="d-flex align-items-center">
+              <FontAwesomeIcon icon={faArrowRight} />
+              <span className="p-2">Đào tạo ngoại ngữ</span>
+            </div>
+            <div className="d-flex align-items-center">
+              <FontAwesomeIcon icon={faArrowRight} />
+              <span className="p-2">Đào tạo tin học</span>
+            </div>
+            <div className="d-flex align-items-center">
+              <FontAwesomeIcon icon={faArrowRight} />
+              <span className="p-2">Luyện thi</span>
+            </div>
+            <div className="d-flex align-items-center">
+              <FontAwesomeIcon icon={faArrowRight} />
+              <span className="p-2">Cấp chứng chỉ CNTT-ngoại ngữ</span>
+            </div>
+          </div>
+        </div>
+        <div className="App-footer-contact">
+          <h6>Mạng xã hội</h6>
+          <div className="d-flex justify-center align-items-center">
+            <div className="d-flex align-items-center">
+              <FontAwesomeIcon
+                icon={faFacebook}
+                size="2x"
+                className="p-2 cursor-pointer"
+              />
+            </div>
+            <div className="d-flex align-items-center">
+              <FontAwesomeIcon
+                icon={faInstagram}
+                size="2x"
+                className="p-2 cursor-pointer"
+              />
+            </div>
+            <div className="d-flex align-items-center">
+              <FontAwesomeIcon
+                icon={faTwitter}
+                size="2x"
+                className="p-2 cursor-pointer"
+              />
+            </div>
+            <div className="d-flex align-items-center">
+              <FontAwesomeIcon
+                icon={faYoutube}
+                size="2x"
+                className="p-2 cursor-pointer"
+              />
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="App-footer-copyright">
+        <span>
+          © 2024 Bản quyền thuộc về Viện VTC. Website được thiết kế bởi Mr.Cong
+          Designer
+        </span>
+      </div>
+    </>
+  );
 }
