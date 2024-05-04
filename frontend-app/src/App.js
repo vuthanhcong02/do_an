2641520e8510862dc3f02484c0cd8380b547371b
congvt <cong.vt@zinza.com.vn>
2024-05-04 17:20:24 +0700

    fix reset password

--- frontend-app/src/App.js
+++ frontend-app/src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.scss";
+import Footer from "./components/Layouts/Footer/Footer";
+import Header from "./components/Layouts/Header/Header";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App-container">
+      <div className="App-header">
+        <Header />
+      </div>
+      <div className="App-content">
+        <div>Content</div>
+      </div>
+      <div className="App-footer">
+        <Footer />
+      </div>
     </div>
   );
 }
---
+++ frontend-app/src/components/Layouts/Header/Header.js
@@ -0,0 +1,101 @@
+import React from "react";
+import Button from "react-bootstrap/Button";
+import Container from "react-bootstrap/Container";
+import Form from "react-bootstrap/Form";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+import NavDropdown from "react-bootstrap/NavDropdown";
+import Offcanvas from "react-bootstrap/Offcanvas";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faPhone, faEnvelope } from "@fortawesome/free-solid-svg-icons";
+import {
+  faInstagram,
+  faTwitter,
+  faYoutube,
+  faFacebook,
+} from "@fortawesome/free-brands-svg-icons";
+
+export default function Header() {
+  return (
+    <>
+      <div className="App-header-contact">
+        <div className="d-flex justify-content-center align-items-center">
+          <div className="App-header-contact-phone d-flex align-items-center">
+            <FontAwesomeIcon icon={faPhone} />
+            <span className="p-2">0971765824</span>
+          </div>
+          <div className="App-header-contact-email d-flex align-items-center">
+            <FontAwesomeIcon icon={faEnvelope} />
+            <span className="p-2">congvtc02@gmail.com</span>
+          </div>
+        </div>
+        <div>
+          <div className="d-flex justify-content-center align-items-center">
+            <FontAwesomeIcon icon={faFacebook} className="p-2" />
+            <FontAwesomeIcon icon={faInstagram} className="p-2" />
+            <FontAwesomeIcon icon={faTwitter} className="p-2" />
+            <FontAwesomeIcon icon={faYoutube} className="p-2" />
+          </div>
+        </div>
+      </div>
+      {["sm"].map((expand) => (
+        <div className="App-header-menu">
+          <Navbar
+            key={expand}
+            expand={expand}
+            className="bg-body-tertiary mb-3"
+          >
+            <Container fluid className="bg-body-tertiary">
+              <Navbar.Brand href="#">Logo</Navbar.Brand>
+              <Navbar.Toggle
+                aria-controls={`offcanvasNavbar-expand-${expand}`}
+              />
+              <Navbar.Offcanvas
+                id={`offcanvasNavbar-expand-${expand}`}
+                aria-labelledby={`offcanvasNavbarLabel-expand-${expand}`}
+                placement="end"
+              >
+                <Offcanvas.Header closeButton>
+                  <Offcanvas.Title id={`offcanvasNavbarLabel-expand-${expand}`}>
+                    Offcanvas
+                  </Offcanvas.Title>
+                </Offcanvas.Header>
+                <Offcanvas.Body className="align-items-center">
+                  <Nav className="justify-content-end flex-grow-1 pe-3">
+                    <Nav.Link href="#action1" active>
+                      Trang chủ
+                    </Nav.Link>
+                    <Nav.Link href="#action2">Tin tức</Nav.Link>
+
+                    <NavDropdown
+                      href="#action2"
+                      title="Đào tạo"
+                      id="offcanvasNavbarDropdown-expand-${expand}"
+                    >
+                      <NavDropdown.Item href="#action3">
+                        Ngoại ngữ
+                      </NavDropdown.Item>
+                      <NavDropdown.Item href="#action4">
+                        Tin tức
+                      </NavDropdown.Item>
+                    </NavDropdown>
+                    <Nav.Link href="#action2">Thông báo</Nav.Link>
+                    <Nav.Link href="#action2">Liên hệ</Nav.Link>
+                  </Nav>
+                  <Form className="d-flex App-header-search-input">
+                    <input
+                      type="search"
+                      placeholder="Tìm kiếm"
+                      aria-label="Search"
+                    />
+                    <button>Tìm kiếm</button>
+                  </Form>
+                </Offcanvas.Body>
+              </Navbar.Offcanvas>
+            </Container>
+          </Navbar>
+        </div>
+      ))}
+    </>
+  );
+}
