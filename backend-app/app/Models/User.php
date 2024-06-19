cfe6ae53c98c92ab3c191ed4f8a21966a4801326
congvt <cong.vt@zinza.com.vn>
2024-06-19 10:29:29 +0700

    Revert "config auth"
    This reverts commit 5be4009757b7a2eef4d3b4a5c9e6bbae020b9e30.

--- backend-app/app/Models/User.php
+++ backend-app/app/Models/User.php
@@ -7,9 +7,8 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
-use Tymon\JWTAuth\Contracts\JWTSubject;
 
-class User extends Authenticatable implements JWTSubject
+class User extends Authenticatable
 {
     use HasApiTokens, HasFactory, Notifiable;
 
@@ -22,12 +21,10 @@ class User extends Authenticatable implements JWTSubject
         'full_name',
         'email',
         'gender',
-        'password',
         'phone',
         'address',
         'object_type',
         'date_of_birthday',
-        'id_card',
     ];
 
     public function registrations()
@@ -53,14 +50,4 @@ class User extends Authenticatable implements JWTSubject
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
-
-    public function getJWTIdentifier()
-    {
-        return $this->getKey();
-    }
-
-    public function getJWTCustomClaims()
-    {
-        return [];
-    }
 }
--- frontend-app/src/utils/axiosCustom.js
+++ frontend-app/src/utils/axiosCustom.js
@@ -13,10 +13,10 @@ const httpClient = axios.create({
 
 httpClient.interceptors.request.use(
   function (config) {
-    // const token = localStorage.getItem("token");
-    // if (token) {
-    //   config.headers.Authorization = `Bearer ${token}`;
-    // }
+    const token = localStorage.getItem("token");
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
     return config;
   },
   function (error) {
