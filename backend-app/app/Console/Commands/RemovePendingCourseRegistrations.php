7d498408eccd6ec786f6c52a44306814c5def845
congvt <cong.vt@zinza.com.vn>
2024-07-19 17:16:33 +0700

    add cronjob remove course register when deadline_date with status pending

---
+++ backend-app/app/Console/Commands/RemovePendingCourseRegistrations.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Models\Registration;
+use Illuminate\Console\Command;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Log as FacadesLog;
+
+class RemovePendingCourseRegistrations extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'app:remove-pending-course-registrations';
+    protected $description = 'Remove pending registrations that have exceeded the deadline date';
+
+    public function __construct()
+    {
+        parent::__construct();
+    }
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+
+    /**
+     * Execute the console command.
+     */
+    public function handle()
+    {
+        //
+        $today = now()->toDateString();
+
+        $registrationsToDelete = DB::table('registrations')
+            ->join('schedules', 'registrations.schedule_id', '=', 'schedules.id')
+            ->join('courses', 'schedules.course_id', '=', 'courses.id')
+            ->where('registrations.status', 'pending')
+            ->where('courses.deadline_date', '<', $today)
+            ->select('registrations.id')
+            ->get();
+        Registration::whereIn('id', $registrationsToDelete->pluck('id'))->delete();
+        // FacadesLog::info('Removed ' . count($registrationsToDelete) . ' pending registrations that have exceeded the deadline date');
+    }
+}
--- backend-app/app/Console/Kernel.php
+++ backend-app/app/Console/Kernel.php
@@ -14,6 +14,7 @@ class Kernel extends ConsoleKernel
     {
         // $schedule->command('inspire')->hourly();
         $schedule->command('assign:exam-shifts')->daily();
+        $schedule->command('app:remove-pending-course-registrations')->daily();
     }
 
     /**
